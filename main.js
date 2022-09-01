a7d1ea9083e0c531c474334103c27be28dbfcd4a
matheushassen <matheushassen@hotmail.com>
2022-09-01 10:43:16 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+   #header`)
+
+ScrollReveal({
+    origin: 'left',
+    distance: '30px',
+    duration: 1000,
+}).reveal(`
+    #card1,
+    #card2,
+    #card3
+`)
+
